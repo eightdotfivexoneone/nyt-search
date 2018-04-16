44dae3ab99072177873fd147511a18124b51de9e
“Steve <graphicdesigner@gmail.com>
2018-04-16 16:10:57 -0400

    25LL hw w/ matt and molly

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,45 +1,39 @@
 $(document).ready(function(){
-  $("button").on("click", function () {
+  $("#search-button").on("click", function () {
   console.log($(this));
   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-  url += '?' + $.param({
-    'api-key': "cb909a8eea0945e795839b3d1ea035f0",
-    'q': "politics",
-  'begin_date': "19910101",
-  'end_date': "20010101"
-  });
-  console.log(url);
-  var searchTerm = $("#term").val();
+  var searchTerm = $("#search-term").val();
   var numRecords = $("#num-records").val();
   var startYear = $("#start-year").val();
   var endYear = $("#end-year").val();
+  url += '?' + $.param({
+    'api-key': "cb909a8eea0945e795839b3d1ea035f0",
+    'q': searchTerm,
+  'begin_date': startYear,
+  'end_date': endYear
+  });
+  console.log(url);
+
  
   $.ajax({
     url: url,
     method: "GET"
   }).then(function(response) {
-    var results = response.response.docs[0];
+
+    for (i = 0; i < numRecords; i++) {
+    var results = response.response.docs[i];
     console.log(results);
-    $(".results").append(results.headline.main);
-    $(".results").append(" " + results.source);
-    $(".results").append(" " + results.web_url);
-  }).then(function(response) {
-    
-    var results = response.response.docs;
- 
-  console.log(results);
- 
-  $("<div id='results'>").append(results);
- 
-  for (var i = 0; i < results.length; i++) {
-    var rank = results.multimedia.rank;
-    var pRank = $("<p></p>").text(rank[i]);
-    var title = results.headline.main;
-    var pTitle = $("<p></p>").text(title[i]);
-    var author = results.multimedia.byline.original;
-    var pAutho = $("<p></p>").text(author[i]);
-  }
+    console.log(searchTerm);
+    console.log(startYear);
+    console.log(endYear);
+    console.log(numRecords);
+    $("#results-1").append("<h2>" + results.headline.main + "</h2>" + "<br>" + results.source + "<a href='" + results.web_url + "'>" + "<br>" + results.web_url + "</a>" + "<br><br>" + "<HR COLOR='white' WIDTH='100%'>");
+
+    }
   });
  });
- 
+ $("#clear-button").on("click", function () {
+  $(".results").html(" ");
  });
+ });
+
